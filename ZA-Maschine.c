786bceff32145f48d76549d61cfeb2d67e9d75d2
mountassir-007 <ahmedelmountassir10@gmail.com>
2021-07-18 23:56:49 +0100

    harder

--- ZA-Maschine.c
+++ ZA-Maschine.c
@@ -2,21 +2,26 @@
 #include <stdio.h>
 #include <unistd.h>
 
-char *rot(char *message, int rotor, int s_p){
-	int i = 0;
-	while(message[i]){
-		int enc = s_p;
-		if(message[i] <= 'z' && message[i] >= 'a'){
-				while(enc > 0){
-					if(message[i]== 'z')
-						message[i] = 'a';
-					else
-						message[i]++;
-					enc--;
-				}
+char *rot(char *message, int rotor, int s_p, int rdm){
+	int i;
+	while(rotor > 0){
+		i = 0;
+		while(message[i]){
+			int enc = s_p;
+			if(message[i] <= 'z' && message[i] >= 'a'){
+					while(enc > 0){
+						if(message[i]== 'z')
+							message[i] = 'a';
+						else
+							message[i]++;
+						enc--;
+					}
+			}
+			s_p += rdm;
+			i++;
 		}
-		s_p += 4;
-		i++;
+		s_p +=12;
+		rotor--;
 	}
 	return message;
 }
@@ -34,18 +39,21 @@ int main(){
 	char choice;
 	int rotor;
 	int enc_st;
+	int rdm;
 	printf("Do you want to ENCr or DECr ? [E] [D]\n");
 	scanf("%s", &choice);
 	if(choice == 'e' || choice =='E'){
 		char message[500];
 		printf("type the message you want to encrypte\n");
 		scanf("%s", message);
-		printf("how many Rotors you want to use? 1-5\n-");
+		printf("how many Rotors you want to use? 3->5\n-");
 		scanf("%d", &rotor);
-		printf("select the encryption stating point 1-16\n-");
+		printf("select the encryption stating point 1->16\n-");
 		scanf("%d", &enc_st);
-		printf("encryption done :\n %s ", rot(message, rotor, enc_st) );
-		printf("your decryption codes \n-R: %d\n-S.P: %d", rotor, enc_st);
+		printf("choose a number between 1->12\n-");
+		scanf("%d", &rdm);
+		printf("\nencryption done :\n %s ", rot(message, rotor, enc_st, rdm) );
+		printf("\nyour decryption codes \n-R: %d\n-S.P: %d\n-R_number: %d", rotor, enc_st, rdm);
 	}
 	else if(choice == 'd'|| choice == 'D'){
 		int code[20];
@@ -53,6 +61,8 @@ int main(){
 		scanf("%d", &rotor);
 		printf("\n2-S.P: ");
 		scanf("%d", &enc_st);
+		printf("\n 3-R_number: ");
+		scanf("%d", &rdm);
 		printf("type the code you want to decrypte\n");
 		scanf("%ls", code);
 	}
